bca5c5539d0aef11cb0e7b9cedc8ee1d12bc3f39
Esc Adn <53090163+EscAdn@users.noreply.github.com>
2023-02-23 10:24:55 -0600

    Add files via upload

---
+++ node.validator.js
@@ -0,0 +1,23 @@
+import { check } from "express-validator";
+import validateResults from "../helpers/validateHelper";
+
+const validateCreate = [
+  check("address_id").exists().isNumeric(),
+  check("details").exists().not().isEmpty().isString(),
+  check("ports")
+    .exists()
+    .isNumeric()
+    .custom((value, { req }) => {
+      if (value === 8 || value === 16) {
+        return true;
+      }
+      throw new Error("El número de puertos no es valido");
+    }),
+  (req, res, next) => {
+    validateResults(req, res, next);
+  },
+];
+
+export const validated = {
+  validateCreate,
+};
